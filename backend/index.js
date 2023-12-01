7a89813e3ca7623f24714726f3ad52f96a0d00da
hatim <htimdady31i@gmailcom>
2023-12-01 18:23:48 +0530

    backend configured

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 const dbURL = process.env.MONGODB_ATLAS;
 
 
-mongoose.connect(dbURL);
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once("open", () => {
